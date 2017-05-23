84e243b0f99ee6545098e72bf55b7aa8a1cdd887
Rebecca Han <xhan061@uottawa.ca>
2017-05-23 15:38:25 -0400

    Update Readme.py

--- Readme.py
+++ Readme.py
@@ -5,7 +5,7 @@ import argparse
 
 #analyze parameter
 ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video", help="path to the video file")
+#ap.add_argument("-v", "--video", help="path to the video file")
 ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
 args = vars(ap.parse_args())
 
